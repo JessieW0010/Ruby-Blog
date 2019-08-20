da3b9a972ad2942d361efef241b65dee07cc406f
JessieW0010 <jessiewang@Jessies-MacBook-Pro.local>
2019-08-20 15:50:08 -0400

    added html for homepage which displays all of the aritcles in the articles database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,20 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
+  def create
+    @article = Article.new(params[:article].permit(:title, :text))
+
+    ## Returns a bool indicating if article was saved or not
+    @article.save 
+    redirect_to @article
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 end
